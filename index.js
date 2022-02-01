492993a69c91a749c9b209e79dc079e1423075bb
Thulio Barbosa <thulio.barbosa0@gmail.com>
2022-02-01 17:09:38 -0300

    Subindo a estrutura e alguns eventos já implementados

---
+++ index.js
@@ -0,0 +1,34 @@
+class Click {
+
+    constructor() {
+
+        this.btnMenu = document.querySelector("#icon-menu");
+        this.header = document.querySelector("#header");
+        this.menuFooter = document.querySelector(".menu-footer");
+        this.logo = document.querySelector("#logo");
+        this.userInfo = document.querySelector(".user-info");
+        this.inputSearch = document.querySelector("#input-search");
+        this.inputGroup = document.querySelector(".input-group");
+        this.main = document.querySelector("#main");
+
+        this.click();
+
+    }
+
+    click() {
+        
+        this.btnMenu.addEventListener( 'click', () => {
+            this.header.classList.toggle("active");
+            this.menuFooter.classList.toggle("active");
+            this.logo.classList.toggle("display-none");
+            this.userInfo.classList.toggle("display-none");
+            this.inputSearch.classList.toggle("display-none");
+            this.inputGroup.classList.toggle("btn-search");
+            this.main.classList.toggle("translate-main");
+        });
+
+    }
+
+}
+
+let click = new Click();
